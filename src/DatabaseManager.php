6e60c1bbdabaa1dddb4e778789aabdec554a9ede
David Kim <woody@woody.co.kr>
2024-06-25 17:32:50 +0900

    Update DatabaseManager.php

--- src/DatabaseManager.php
+++ src/DatabaseManager.php
@@ -4,7 +4,7 @@ class DatabaseManager {
     protected array $_host;
     public function __construct() { return $this->clear(); }
     public function clear() : self { $this->_client = array(); $this->_host = array(); return $this;}
-    public function addHost(string $name) : self { $this->_host[$name] = new DatabaseStructHost(); return $this; }
+    public function addHost(string $name) : ?DatabaseStructHost { $this->_host[$name] = new DatabaseStructHost(); return $this->_host[$name] ?? null; }
     public function getHost(string $name) : ?DatabaseStructHost { return $this->_host[$name] ?? null; }
     public function getClient(string $name) : ?DatabaseClient { return new DatabaseClient($this->_host[$name]) ?? null; }
 }
